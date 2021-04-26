b914d436968ab7b41097318a16b56d7daad44f90
Seonggu Kim <sdkfile@Seongguui-MacBookPro.local>
2021-04-26 11:15:52 +0900

    text->name error fix

--- blender/source/blender/makesrna/intern/rna_nodetree.c
+++ blender/source/blender/makesrna/intern/rna_nodetree.c
@@ -4216,8 +4216,8 @@ static void rna_CompositorNodeGlsl_mode_set(PointerRNA *ptr, int value)
     if (node->id) {
       Text *text = (Text *)node->id;
 
-      if (value == NODE_GLSL_EXTERNAL && text->name) {
-        BLI_strncpy(nglsl->filepath, text->name, sizeof(nglsl->filepath));
+      if (value == NODE_GLSL_EXTERNAL && text->id) {
+        BLI_strncpy(nglsl->filepath, text->id, sizeof(nglsl->filepath));
         BLI_path_rel(nglsl->filepath, BKE_main_blendfile_path_from_global());
       }
 
