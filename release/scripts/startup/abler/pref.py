8f1531ec6a32169778bfab18ca811f68c79bd505
hoie <hoie@acon3d.com>
2021-09-02 11:42:30 -0700

    add pref

--- release/scripts/startup/abler/pref.py
+++ release/scripts/startup/abler/pref.py
@@ -5,19 +5,25 @@ from bpy.app.handlers import persistent
 @persistent
 def init_setting(dummy):
     prefs = bpy.context.preferences
-    pref_sys = prefs.system
+    prefs_sys = prefs.system
     prefs_view = prefs.view
+    prefs_paths = prefs.filepaths
     init_screen = bpy.data.screens['ACON3D'].areas[0].spaces[0]
     init_screen.show_region_header = False
+    init_screen.show_region_tool_header = False
     init_screen.show_gizmo = True
-    init_screen.show_gizmo_navigate = True
+    init_screen.show_gizmo_object_translate = True
+    init_screen.show_gizmo_object_rotate = True
+    init_screen.show_gizmo_object_scale = True
+    init_screen.show_gizmo_navigate = False
     init_screen.show_gizmo_tool = True
     init_screen.show_gizmo_context = True
-    pref_sys.use_region_overlap = False
+    prefs_sys.use_region_overlap = False
     prefs_view.show_layout_ui = True
     prefs_view.show_navigate_ui = False
     prefs_view.show_developer_ui = False
     prefs_view.show_tooltips_python = False
+    prefs_paths.use_load_ui = False
 
 
 def register():
